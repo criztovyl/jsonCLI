d0672a7b617c9cb59ac7cd83b9dcddba536f3ce7
Christoph "criztovyl" Schulz <ch.schulz@joinout.de>
2015-05-14 22:35:50 +0200

    Shebang, no input and inspecting JSON objects
    This commit fixes and the Shebang, moves it above the header (was below wrongly)
    It also introduces JSON object inspection, usable with the first argument "inspect". This will
    display object keys, array lengths or the values and their types.
    Furthermore now no input is also possible, previously this broke the program.

--- jsonCLI.py
+++ jsonCLI.py
@@ -1,4 +1,5 @@
-"""
+#!/usr/bin/python3
+'''
     A command line tool for JSON
     Copyright (C) 2015 Christoph "criztovyl" Schulz
 
@@ -14,20 +15,42 @@
 
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
-"""
-#!/usr/bin/python3
+'''
 import json
 import sys
 input=""
 for line in sys.stdin:
     input = input + line.strip();
+if( input == ""):
+    input="{}"
 json_data = json.loads(input)
 args = sys.argv
 args.pop(0)
+action=""
+if(len(args) > 0 and args[0] in ("inspect")):
+    action = args.pop(0)
 for arg in args:
     try:
         arg = int(arg)
     except:
         pass
-    json_data = json_data[arg]
-print(json_data)
+    try:
+        json_data = json_data[arg]
+    except (IndexError, KeyError, TypeError) as e:
+        print("%s: %s" % (type(e).__name__, e))
+        sys.exit(1)
+#print("JSON data:")
+#print("Action:")
+#print(action)
+if(action == "inspect"):
+    json_type=type(json_data)
+    if(json_type is list):
+        print("It's a list with length %i." % len(json_data))
+    elif(json_type is dict):
+        print("It's a dict with this keys: ")
+        print(" ".join(json_data.keys()))
+    else:
+        print("Type %s" % json_type.__name__)
+        print(json_data)
+else:
+    print(json.dumps(json_data, sort_keys=True, indent=4))
